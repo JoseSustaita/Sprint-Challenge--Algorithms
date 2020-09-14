93f655bf75e2f0a4ae2f0b9da91125fe92f88528
Jose Sustaita <josesustaita70@gmail.com>
2020-09-13 21:18:00 -0400

    mvp

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -16,6 +16,6 @@ c) O(n)
 
 ## Exercise II
 
-I will use a binary search algorithim and the runtime complexity for this would be O(logn)
+I would use a binary search algorithim and the runtime complexity for this would be O(logn)
 
 This makes sense because you want to elimate half the floors right off the bat to find which floor is the highest point you can drop off an egg without breaking. Running this will get you the middle floor fast and then from there you can test if the eggs breaks above or below that.
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,20 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
+
+    # If word is less than 2, it does not contain
+    if len(word) < 2:
+        return 0
+
+# If first two letters are th return 1 contain with the other letters
+    if word[:2] == "th":
+        return 1 + count_th(word[2:])
+
+# Otherwise continue with next letter
+    else:
+        return count_th(word[1:])
+
     pass
